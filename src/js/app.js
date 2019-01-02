7d7588a62c34eb5d0ca464013f3b841b0c0f75b6
oran rozenbaum <oran.rozenbaum@gmail.com>
2019-01-02 20:14:13 +0200

    Revert "first commit"
    This reverts commit d5097d0e399c2c8ef319fd1fb8ba193ec5acb0fb.

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,7 @@
 import $ from 'jquery';
 import {parseCode} from './code-analyzer';
 import * as sym from './part2';
-import * as gra from './graph';
-export {initAll,startGraph};
+
 
 
 $(document).ready(function () {
@@ -14,30 +13,19 @@ $(document).ready(function () {
         let parsedCode = parseCode(codeToParse);
         let input = $('#inputNum').val();
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
-        initAll();
+        sym.initCounter();
+        sym.initTable();
+        index = 0;
+        sym.initMapV();
         sym.inputVariable(input);
         sym.start(parsedCode);
-        let arrNode = sym.getArrNode();
-        startGraph(arrNode);
-        let strGraph = gra.getStrGraph();
-        draw(strGraph);
+        let finalCode = sym.validationCode(codeToParse);
+        printColorCode(finalCode);
         JSON.stringify(parsedCode);
     });
 });
-function initAll() {
-    sym.initCounter();
-    sym.initTable();
-    sym.initNode();
-    sym.initMapV();
-}
-function startGraph(arrNode) {
-    gra.initStrGraph();
-    gra.buildNodesGraph(arrNode[0]);
-    gra.buildEdgesGraph(arrNode[0]);
-    gra.addEdgeBtwnCircle();
-}
-/*let index = 0;
 
+let index = 0;
 function printColorCode(code){
     let colorMap = sym.getColorMap();
     for(let i = 0;i<code.length;i++ ){
@@ -58,23 +46,4 @@ function checkColor(temp) {
         return 'green';
     else if (temp === false)
         return 'red';
-}*/
-function draw(str) {
-    var diagram = flowchart.parse(str);
-    diagram.drawSVG('diagram', {
-        'x': 0, 'y': 0,
-        'line-width': 2, 'line-length': 50, 'text-margin': 10, 'font-size': 12, 'font-color': 'black', 'line-color': 'black',
-        'element-color': 'black', 'fill': 'white', 'yes-text': 'T', 'no-text': 'F', 'arrow-end': 'block', 'scale': 1,
-        'symbols': {
-            'start': {
-                'font-color': 'black', 'element-color': 'green', 'fill': 'yellow'
-            },
-            'end':{
-                'class': 'end-element'
-            }
-        },
-        'flowstate' : {
-            'green' : { 'fill' : 'green'}, 'white': {'fill' : 'white'}
-        }
-    });
 }
